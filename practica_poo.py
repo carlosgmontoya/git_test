51fbdd99f37499e9256c3840266eb742362b0f4f
carlos <carlosg.montoya@gmail.com>
2026-07-03 16:54:52 -0500

    POO con herencia

--- practica_poo.py
+++ practica_poo.py
@@ -16,10 +16,25 @@ class Auto:
         print(f"El auto {self.marca} {self.modelo} está tocando la bocina: ¡BEEP BEEP!")
 
 
+class Camion(Auto):
+    def bombear_agua(self):
+        print(f"El camión {self.marca} {self.modelo} está lanzando agua.")
+
+class Motocicleta(Auto):
+    def __init__(self, marca, modelo, color, cilindrada):
+        super().__init__(marca, modelo, color)
+    def hacer_caballito(self):
+        print(f"La motocicleta {self.marca} {self.modelo} está haciendo un caballito.")
+
 toyota = Auto("Toyota", "Corolla", "Rojo")
 nissan = Auto("Nissan", "Altima", "Azul")
 hyundai = Auto("Hyundai", "Elantra", "Negro")
+bombero = Camion("Bombero", "B-200", "Rojo")
+yamaha = Motocicleta("Yamaha", "YZF-R3", "Azul", 321)
 
 toyota.encender()
-toyota.tocar_bocina()
+nissan.tocar_bocina()
+bombero.bombear_agua()
+yamaha.hacer_caballito()
+
 
